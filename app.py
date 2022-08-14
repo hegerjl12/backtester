c09c3ad6b461a0c9eacac26881be1a2d491c3c57
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-08-13 19:35:52 -0700

    test upload print

--- app.py
+++ app.py
@@ -5,11 +5,25 @@ import numpy as np
 
 def main():
 
-  st.set_page_config(
-     page_title="Back Tester",
-     page_icon="🧮")
+    st.set_page_config(
+        page_title="Back Tester",
+        page_icon="🧮")
 
+    st.title("TradingView Back Tester")
 
+    raw_data = st.file_uploader("Upload TradingView Chart Export")
+
+    if data is not None:
+
+        try:
+
+            panda_data = pd.read_csv(raw_data)
+            numpy_data = panda_data.iloc[2:,1:].to_numpy()
+    
+            st.dataframe(numpy_data)
+            
+        except ValueError:
+            st.warning("Upload Error")
 
 if __name__ == "__main__":
     main()

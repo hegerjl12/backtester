84ce49100a3ea0fdf9b686583b7f21affe49805e
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-08-13 19:20:44 -0700

    setup

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import streamlit as st
 import numpy as np
 
 
+def main():
+
+  st.set_page_config(
+     page_title="Back Tester",
+     page_icon="🧮")
+
+
+
+if __name__ == "__main__":
+    main()

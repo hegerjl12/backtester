2cd6588b985f41b1193b4df4d996edabfe915f74
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-09-22 14:39:13 -0700

    pd

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def main():
                     adj_in = trade * (1+commission_adjustment)
                     portfolio_value -= adj_in
                
-                if not np.isnan(row[25]):
+                if not pd.isna(row[25]):
                     sell_price = row[sell_value]
                     st.write(sell_price)
                     trading = False

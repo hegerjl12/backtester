fc0e2711c86d6330be736e17e158eeb544539900
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-09-22 15:15:22 -0700

    try

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def main():
             planned_share_purchase = st.number_input('Enter Number of Shares Trading', min_value=2)
 
             for row in numpy_data:
-                if row[24] != 'nan':
+                if not np.isnan(row[24]):
                     buy_price = row[buy_value]
                     st.write(row)
                     st.write(buy_price)
@@ -62,7 +62,7 @@ def main():
                     adj_in = trade * (1+commission_adjustment)
                     portfolio_value -= adj_in
                
-                if row[25] != 'nan':
+                if not np.isnan(row[25]):
                     sell_price = row[sell_value]
                     st.write(sell_price)
                     trading = False
